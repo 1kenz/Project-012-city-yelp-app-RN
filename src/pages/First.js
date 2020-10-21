4a814298ca271016c2083fd5d21e1e28fb9596b5
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-22 01:26:00 +0300

    Update First.js

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,24 +1,61 @@
-import React, { useState } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Alert,
+  TextInput,
+  Button,
+} from 'react-native';
+
+import restaurant_data from '../restaurant_data.json';
+// import {ProductCard, SearchBar} from './components';
 
 export default function First(props) {
-  const [userName, setUserName] = useState("");
-  const number = 22;
+  const [cityName, setCityName] = useState('');
+  const [onSearch, setOnSearch] = useState('');
+  const sendValue = () => {
+    props.getName(onSearch);
+
+    const [displayList, setDisplayList] = useState([]);
+
+    // const renderListItem = ({ item }) => <ProductCard product={item} />;
+    //   setDisplayList(productData);
+    // }, []);
+
+    useEffect(() => {
+      const filteredValue = productData.filter((item) => {
+        const text = searchValue.toUpperCase();
+        const productTitle = item.title.toUpperCase();
+        return productTitle.indexOf(text) > -1;
+      });
+      setDisplayList(filteredValue);
+    }, [searchValue]);
+  };
 
   return (
     <View>
-      <Text style={{ fontSize: 40 }}>First</Text>
-      <Text style={{ fontSize: 40 }}>{number}</Text>
-      <TextInput value={userName} onChangeText={(text) => setUserName(text)} />
+      <Text>Bir şehir seçiniz</Text>
+      <TextInput
+        value={onSearch}
+        placeholder="Search city..."
+        onChangeText={(val) => {
+          setOnSearch(val);
+          sendValue();
+        }}
+      />
+      <FlatList
+        keyExtractor={(_, index) => index.toString()}
+        data={displayList}
+        renderItem={renderListItem}
+        numColumns={2}
+      />
+
       <Button
         title="Second Page"
-        onPress={() =>
-          props.navigation.navigate("SecondPage", {
-            myNumber: number,
-            name: "ken",
-            selectedValue: userName,
-          })
-        }
+        onPress={() => props.navigation.navigate('SecondPage', {})}
       />
     </View>
   );
