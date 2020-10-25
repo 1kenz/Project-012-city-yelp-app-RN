4498e34d180a950ddbb5a0d435ca1454cf8f21fb
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-25 21:22:04 +0300

    update

--- src3/pages/CityList.js
+++ src3/pages/CityList.js
@@ -1,65 +1,63 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { SafeAreaView, View, Text, FlatList } from "react-native";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
 
-import { CityItem, SearchBar } from "../components";
+import { CityItem, SearchBar } from '../components'
 
-let originalList = [];
+let originalList = []
 
 const CityList = (props) => {
-  const [cityList, setCityList] = useState([]);
+    const [cityList, setCityList] = useState([]);
 
-  // ASYNC-AWAIT
-  const fetchCityData = async () => {
-    const { data } = await axios.get(
-      "https://opentable.herokuapp.com/api/cities",
-    );
-    setCityList(data.cities);
-    originalList = [...data.cities];
-  };
+    // ASYNC-AWAIT 
+    const fetchCityData = async () => {
+        const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
+        setCityList(data.cities);
+        originalList = [...data.cities];
+    }
 
-  useEffect(() => {
-    fetchCityData();
-  }, []);
+    useEffect(() => {
+        fetchCityData();
+    }, [])
 
-  const renderCities = ({ item }) => {
-  return (
-  <CityItem 
-  cityName={item}
-  onSelect={() => props.navigation.navigate("Restaurants", {selectedCity: item})}
-  )} />;
-  
-  const renderSeperator = () => (
-    <View style={{ borderWidth: 1, borderColor: "#e0e0e0" }} />
-  );
+    const renderCities = ({ item }) => {
+        return (
+            <CityItem
+                cityName={item}
+                onSelect={() => props.navigation.navigate('Restaurants', { selectedCity: item })}
+            />
+        )
+    }
 
-  function searchCity(search) {
-    const filteredCities = originalList.filter((city) => {
-      const text = search.toUpperCase();
-      const cityName = city.toUpperCase();
+    const renderSeperator = () => <View style={{ borderWidth: 1, borderColor: '#e0e0e0' }} />
 
-      return cityName.indexOf(text) > -1;
-    });
+    function searchCity(search) {
+        const filteredCities = originalList.filter(city => {
+            const text = search.toUpperCase();
+            const cityName = city.toUpperCase();
 
-    setCityList(filteredCities);
-  }
+            return cityName.indexOf(text) > -1;
+        })
 
-  return (
-    <SafeAreaView>
-      <View>
-        <SearchBar
-          placeholder="Search a city..."
-          onSearch={(value) => searchCity(value)}
-        />
-        <FlatList
-          keyExtractor={(_, index) => index.toString()}
-          data={cityList}
-          renderItem={renderCities}
-          ItemSeparatorComponent={renderSeperator}
-        />
-      </View>
-    </SafeAreaView>
-  );
-};
+        setCityList(filteredCities);
+    }
+
+    return (
+        <SafeAreaView>
+            <View>
+                <SearchBar
+                    placeholder="Search a city..."
+                    onSearch={(value) => searchCity(value)}
+                />
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={cityList}
+                    renderItem={renderCities}
+                    ItemSeparatorComponent={renderSeperator}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
 
 export { CityList };
