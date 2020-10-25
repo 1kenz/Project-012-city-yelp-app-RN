8086dafcca88ffec668dc1318b7435619ef71e3f
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-25 19:54:30 +0300

    feat: add searchbar

--- src3/components/SearchBar.js
+++ src3/components/SearchBar.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 const SearchBar = (props) => {
-  return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder={props.placeholder}
-        onChangeText={(value) => props.onSearch(value)}
-      />
-    </View>
-  );
-};
+    return (
+        <View style={styles.container}>
+            <TextInput
+                placeholder={props.placeholder}
+                onChangeText={(value) => props.onSearch(value)}
+            />
+        </View>
+    )
+}
 
-export { SearchBar };
+export { SearchBar }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#e0e0e0",
-    margin: 5,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-  },
-});
+    container: {
+        backgroundColor: '#e0e0e0',
+        margin: 5,
+        padding: 5,
+        borderRadius: 5
+    }
+})
